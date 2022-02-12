465b034f5f2059daf3db1d596ac1da2da33ea113
VivekChaturvedi205 <vivekchaubey205@gmail.com>
2022-02-12 15:37:04 +0530

    This is My First Comment

---
+++ Request.py
@@ -0,0 +1,21 @@
+# GET
+import requests
+import json
+# URL="http://localhost:8000/"
+
+# r=requests.get(url=URL)
+# data=r.json()
+# print(data)
+
+# POST
+
+URL = "http://localhost:8000/stucreate/"
+data = {
+    'Name': 'Sonam',
+    'Roll_no': 101,
+    'City': "Khalilabad"
+}
+json_data = json.dumps(data)
+r = requests.post(url=URL, data=json_data)
+data = r.json()
+print(data)
